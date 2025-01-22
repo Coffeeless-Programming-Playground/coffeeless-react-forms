709c281cc849f05154fb09364f60bcd6c88f98fa
DiegoSalas27 <dominicsc2hs@gmail.com>
2025-01-22 00:18:00 -0500

    build: update tsconfig.json and package.json to streamline transpilation process

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as CForm } from './form';
-export { default as CInput } from './input';
-export { default as CFormCardContainer } from './form-card-container';
-export { default as CFormGroup } from './form-group';
-export { default as CSection } from './section';
-export { default as PrimaryBtn } from './buttons/primary-button';
-export { default as SecondaryBtn } from './buttons/secondary-button';
--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,165 @@
 import React, { ChangeEvent, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Builder, CForm, CFormGroup, CInput, CSection } from '.'
-import classes from './core.module.css'
+import { Builder, CForm, CFormCardContainer, CFormGroup, CInput, CSection } from '.';
 import './css/main.css';
 import { CoffeelessWrapper } from './validator'
 
-interface EmailForm {
+type Props = {
+  onChange: any
+  value?: any
+  id: string
   name: string
-  email: string
-  subject: string
-  body: BodyPart
+  label?: string
+  disabled?: boolean
+  labelStyle?: any
+  selectStyle?: any
+}
+export interface FullReference {
+  id?: number
+  lastName: string
+  title: string
+  publisher?: string
+  isbn?: string
+  url: string
+  firstName: string
+  year: string
+  location?: string
+  pages?: string
+  referenceType: 'WEB' | 'BOOK'
 }
 
-interface BodyPart {
-  part1: string
-  part2: string
+export interface Post {
+  id: number
+  createdAt: string
+  updatedAt: string
+  upNextUrl: string
+  upNextName: string
+  previousUrl: string
+  previousName: string
+  portraitImage: string
+  name: string
+  abs: string | null
+  moduleTopics: string
+  slug: string
+  postContent: PostContent
+  type: string | null
+  tags: Tag[]
+  postFullReferences: Array<{
+    fullReference: FullReference
+    pages: string | null
+  }>
+  posted: boolean
+  indexableList: string | null
+}
+
+export interface PostContent {
+  title: string
+  description: string
+  postAdditionals: PostAdditionals
+  postBody: PostBody
+}
+
+export interface PostAdditionals {
+  type: string | null
+  placeholder: string
+}
+
+export interface PostBody {
+  name: string
+  description: string
+}
+
+export interface Tag {
+  id?: number
+  name: string
+  slug: string
+}
+
+export interface SavePostDto extends Post {
+  previousIndexableList: string[],
+  updateEntries: string[],
+  metadataListToUpdate: string[]
+}
+
+const SelectBox: React.FC<Props & { children: any }> = ({
+  id,
+  name,
+  onChange,
+  value,
+  label,
+  disabled,
+  children,
+  labelStyle,
+  selectStyle
+}) => {
+  return (
+    <>
+      {label && (
+        <label htmlFor={id} style={labelStyle}>
+          {label}
+        </label>
+      )}
+      <select
+        disabled={disabled}
+        className="form-control"
+        id={id}
+        onChange={onChange}
+        name={name}
+        value={value}
+        style={selectStyle}
+      >
+        {children}
+      </select>
+    </>
+  )
 }
 
 const Contact: React.FC = () => {
-  const [form, setForm] = useState<EmailForm>({
+  const [state, setForm] = useState<SavePostDto>({
+    id: 0,
+    createdAt: '',
+    updatedAt: '',
+    portraitImage: '',
     name: '',
-    body: {
-      part1: '',
-      part2: ''
+    tags: [],
+    postFullReferences: [],
+    upNextName: '',
+    upNextUrl: '',
+    abs: '',
+    moduleTopics: '',
+    postContent: {
+      description: '',
+      postAdditionals: {
+        placeholder: '',
+        type: 'SEARCH'
+      },
+      postBody: {
+        description:
+          '<h1 class="editor-text">An introduction to computing</h1><p class="editor-text graf--p">Paragraph 2</p>',
+        name: ''
+      },
+      title: ''
     },
-    email: '',
-    subject: ''
+    previousName: '',
+    previousUrl: '',
+    slug: '',
+    type: '',
+    posted: false,
+    indexableList: null,
+    previousIndexableList: [],
+    updateEntries: [],
+    metadataListToUpdate: []
   })
 
   useEffect(() => {
-    console.log(form)
+    console.log(state)
   })
 
   function onChange(event: ChangeEvent<HTMLInputElement>) {
     const { name, value } = event.target
 
     const updatePath: string[] = name.split('.')
-    let copy = form
+    let copy = state
 
     updatePath.forEach((path, idx) => {
       if (idx === updatePath.length - 1) {
@@ -46,79 +169,127 @@ const Contact: React.FC = () => {
       }
     })
 
-    setForm({ ...form })
+    setForm({ ...state })
   }
 
   return (
-    <CoffeelessWrapper<EmailForm>
-      initialValues={form}
+    <CoffeelessWrapper<Post>
+      initialValues={state}
       validationSchema={{
-        body: {
-          part1: Builder.init().required().min(2).build(),
-          part2: Builder.init().required().min(2).build()
-        },
-        email: Builder.init().required().min(5).email().build(),
-        name: Builder.init().required().min(5).build(),
-        subject: Builder.init().required().min(5).build()
+        name: Builder.init().required().min(2).build(),
+        portraitImage: Builder.init().required().build(),
+        abs: Builder.init().required().min(100).build(),
+        postContent: {
+          title: Builder.init().required().min(2).build(),
+          description: Builder.init().required().min(2).build(),
+          postAdditionals: {
+            placeholder: Builder.init().required().min(2).build()
+          },
+          postBody: {
+            description: Builder.init().required().min(2).build()
+          }
+        }
+      }}
+      skipSchemaValidation={{
+        portraitImage: state.type !== 'ARTICLE',
+        postContent: {
+          postAdditionals: {
+            placeholder: state.postContent.postAdditionals.type === 'NONE',
+          }
+        }
       }}
     >
-      <CForm handleSubmit={() => {}} submitText="Submit" formWrapperClass={classes.row} includeFormButtons>
-        <CSection col="col-md-6">
-          <CFormGroup>
-            <CInput
-              id="name"
-              label="Name"
-              name="name"
-              onChange={onChange}
-              placeholder="Enter you name"
-              type="text"
-              value={form.name}
-            />
-          </CFormGroup>
-          <CFormGroup>
-            <CInput
-              id="email"
-              label="Email"
-              name="email"
-              onChange={onChange}
-              placeholder="Enter your email"
-              type="email"
-              value={form.email}
-            />
-          </CFormGroup>
-          <CFormGroup>
-            <CInput
-              id="subject"
-              label="Subject"
-              name="subject"
-              onChange={onChange}
-              placeholder="Enter subject"
-              type="text"
-              value={form.subject}
-            />
-          </CFormGroup>
-          <CFormGroup>
-            <CInput
-              id="BodyPart1"
-              label="BodyPart1"
-              name="body.part1"
-              onChange={onChange}
-              placeholder="Enter message here"
-              type="textarea"
-              value={form.body.part1}
-            />
-          </CFormGroup>
-          <CFormGroup>
-            <CInput
-              id="BodyPart2"
-              label="BodyPart2"
-              name="body.part2"
-              onChange={onChange}
-              placeholder="Enter message here"
-              type="textarea"
-              value={form.body.part2}
-            />
-          </CFormGroup>
+      <CForm
+        handleSubmit={() => {console.log('Submitted')}}
+        handleCancel={() => {}}
+        submitText={'Submit'}
+        includeFormButtons
+      >
+        <CSection col="col-md-12">
+          <CFormCardContainer cardTitle="Post navbar" cardDescription="heading info">
+            <CFormGroup>
+              <CInput
+                id="title"
+                label="Title"
+                name="postContent.title"
+                onChange={onChange}
+                placeholder="Title for the post"
+                type="text"
+                value={state.postContent.title}
+              />
+            </CFormGroup>
+            <CFormGroup>
+              <CInput
+                id="description"
+                label="Description"
+                name="postContent.description"
+                onChange={onChange}
+                placeholder={'Description'}
+                type="text"
+                value={state.postContent.description}
+              />
+            </CFormGroup>
+            <CFormGroup>
+              <SelectBox
+                // disabled
+                label="Type"
+                id="select-additional-type"
+                onChange={onChange}
+                name="postContent.postAdditionals.type"
+                value={state.postContent.postAdditionals!.type}
+              >
+                {[{value: 'LIST_IMAGE'}, {value: 'SEARCH'}, {value: 'NONE'}].map(lovDetail => (
+                  <option key={lovDetail.value} value={lovDetail.value}>
+                    {lovDetail.value}
+                  </option>
+                ))}
+              </SelectBox>
+            </CFormGroup>
+            <CFormGroup>
+              <CInput
+                id="placeholder"
+                label="Placeholder"
+                name={'postContent.postAdditionals.placeholder'}
+                onChange={onChange}
+                placeholder={'enter placeholder'}
+                type="text"
+                value={state.postContent.postAdditionals.placeholder}
+              />
+            </CFormGroup>
+          </CFormCardContainer>
+
+          <CFormCardContainer
+            cardTitle="Post content"
+            cardDescription="write your post"
+          >
+            <CFormGroup>
+              <CInput
+                id="abstract"
+                label="Abstract"
+                name={'abs'}
+                onChange={onChange}
+                placeholder={'enter abstract'}
+                type="textarea"
+                value={state.abs}
+                rows={4}
+              />
+            </CFormGroup>
+            <CFormGroup>
+              <CInput
+                id="moduleTopics"
+                label="Module topics"
+                name="moduleTopics"
+                onChange={onChange}
+                placeholder="module topics"
+                type="textarea"
+                rows={5}
+                value={state.moduleTopics}
+              />
+            </CFormGroup>
+            <CFormGroup>
+              <label htmlFor="select-additional-type">Description</label>
+            </CFormGroup>
+          </CFormCardContainer>
         </CSection>
       </CForm>
     </CoffeelessWrapper>
